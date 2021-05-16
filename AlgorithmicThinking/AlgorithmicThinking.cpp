4a7f51b0f90859aa2aafb4f8dccf8faa4e1e2e69
Sibi Chakravarthy P <66554485+SibiChakravarthy7311@users.noreply.github.com>
2021-05-16 11:43:11 +0530

    Peak FInder Problem

--- AlgorithmicThinking/AlgorithmicThinking.cpp
+++ AlgorithmicThinking/AlgorithmicThinking.cpp
@@ -2,9 +2,10 @@
 //
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
-
+/*
 class Node {
 public:
     int data;
@@ -37,19 +38,168 @@ Node* LCA(Node* root, int n1, int n2) {
     }
     return LCA(root->right, n1, n2);
 }
+*/
 
 int main()
 {
-    Node* root = new Node(1);
-    root->left = new Node(2);
-    root->right = new Node(3);
-    root->left->left = new Node(4);
-    root->left->right = new Node(5);
-    root->right->left = new Node(6);
-    root->right->right = new Node(7);
+    /*
+    Peak Finder problem
+    Find a peak if it exists in a given array
+    1D array, arr = [a,b,c,d,e,f,g,h,i]
+	position 2 is a peak if b>=a && b>=c
+	position 9 is a peak if i>h
+    */
 
-    cout << "LCA: " << LCA(root, 2, 5)->data;
+    vector<int> v = {4,7,2,3,8,9,0,1,5};
+    
+    //vector<int> v = { 0,1,2,3,4,5,6,7,8,9 };
+    bool peak = false;
+    int h = v.size(), l=0, ci, i=0, s;
+    s = h;
 
+    //1D-peak Straight forward approach
+    
+    for (i = 0; i < h; i++) {
+        if (i == 0) {
+            if (v[i] > v[i + 1]) {
+                peak = true;
+                break;
+            }
+        }
+        else if (i == h-1) {
+            if (v[i] > v[i - 1]) {
+                peak = true;
+                break;
+            }
+        }
+        else {
+            if (v[i]>=v[i - 1] && v[i]>=v[i+1]) {
+                peak = true;
+                break;
+            }
+        }
+    }
+    if (peak) {
+        cout << "StraightForward Algorithm 1D-Peak: " << v[i] << endl;
+        peak = false;
+    }
+
+    //1D-peak binary search approach
+
+    while (true) {
+        ci = (h + l) / 2;
+        if (ci == 0 || ci == s - 1) {
+            peak = true;
+            break;
+        }
+        if (v[ci] < v[ci - 1]) {
+            h = ci;
+        }
+        else if (v[ci] < v[ci + 1]) {
+            l = ci;
+        }
+        else {
+            peak = true;
+            break;
+        }
+    }
+    if (peak) {
+        cout << "Binary Search 1D-Peak: " << v[ci] << endl;
+        peak = false;
+    }
+    
+    //vector<vector<int>> v2 = { {1,2,3,4}, {14,13,12,5}, {15,9,11,17}, {16,17,19,20} };
+    //vector<vector<int>> v2 = { {1,2,3,4}, {13,12,14,5}, {15,9,11,17}, {16,12,13,20} };
+    vector<vector<int>> v2 = { {1,2,3,4}, {13,14,12,5}, {15,9,11,17}, {16,17,19,20} };
+    int ch = v2[0].size(), cc, cl=0, r=v2.size(), j=0, cmax, imax=0, var;
+    int cs = ch, check=0;
+
+    //2D-peak Straight forward approach
+
+    for (i = 0; i < r; i++) {
+        if (peak) {
+            break;
+        }
+        for (j = 0; j < cs; j++) {
+            check = 0;
+            var = v2[i][j];
+            if (j == 0) {
+                check++;
+            }
+            else {
+                if (var >= v2[i][j - 1]) {
+                    check++;
+                }
+            }
+            if(j==r-1) {
+                check++;
+            }
+            else {
+                if (var >= v2[i][j + 1]) {
+                    check++;
+                }
+            }
+            if (i == 0) {
+                check++;
+            }
+            else {
+                if (var >= v2[i-1][j]) {
+                    check++;
+                }
+            }
+            if (i == cs - 1) {
+                check++;
+            }
+            else {
+                if (var >= v2[i+1][j]) {
+                    check++;
+                }
+            }
+            if (check == 4) {
+                peak = true;
+                break;
+            }
+        }
+    }
+    
+    if (peak) {
+        peak = false;
+        cout << "2D-peak StraightForward algorithm: " << v2[i-1][j] << endl;
+    }
+
+    //2D-peak binary search approach
+
+    while (true) {
+        cc = (ch + cl) / 2;
+        cmax = v2[0][cc];
+        for (i = 0; i < r; i++) {
+            if (cmax < v2[i][cc]) {
+                cmax = v2[i][cc];
+                imax = i;
+            }
+        }
+        if (cc == 0 || cc == cs - 1) {
+            peak = true;
+            break;
+        }
+        if (v2[imax][cc] < v2[imax][cc - 1]) {
+            ch = cc;
+        }
+        else if (v2[imax][cc] < v2[imax][cc + 1]) {
+            cl = cc;
+        }
+        else {
+            peak = true;
+            break;
+        }
+    }
+
+    if (peak) {
+        cout << "2D-peak Binary algorithm: " << v2[imax][cc];
+        peak = false;
+    }
+
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
